38ba7550062fa477d0f07a1b3d935c5687c32b0d
SergioRodVil <srodvil@estudiantec.cr>
2025-03-28 13:57:48 -0600

    Trigger de manejo de inventario

--- P1BDII.sql
+++ P1BDII.sql
@@ -49,3 +49,44 @@ CREATE TABLE Reservas (
     FOREIGN KEY (ID_Usuario) REFERENCES Usuario(ID_Usuario),
     FOREIGN KEY (ID_Libro) REFERENCES Libros(ID_Libro)
 );
+
+
+/*
+--Trigger para manejar inventario al añadir o eliminar libros de la reserva
+CREATE OR REPLACE TRIGGER t_actualizar_invetario
+    BEFORE INSERT OR DELETE
+    ON Libro_Reserva
+    FOR EACH ROW
+DECLARE
+    v_inventario_actual INTEGER;
+    v_titulo VARCHAR(100);
+
+    BEGIN
+        IF inserting THEN
+            SELECT Inventario, Titulo INTO v_inventario_actual, v_titulo
+            FROM Libros
+            WHERE ID_Libro = :NEW.ID_Libro;
+
+            IF v_inventario_actual <= 0 THEN
+                RAISE_APPLICATION_ERROR(-20001, 'Error 20001: No se cuenta con inventario del título' || v_titulo);
+
+            ELSE
+            UPDATE Libros
+                SET Inventario = Inventario - 1
+            WHERE ID_Libro  = :NEW.ID_Libro;
+            END IF;
+
+        ELSIF deleting THEN
+            UPDATE Libros
+                SET Inventario = Inventario + 1
+            WHERE ID_Libro = :OLD.ID_Libro;
+        END IF;
+    EXCEPTION 
+        WHEN NO_DATA_FOUND THEN
+            RAISE_APPLICATION_ERROR(-20002, 'Error 20002: Error al buscar el libro con el ID ' || :NEW.ID_Libro);
+        WHEN OTHERS THEN
+            RAISE_APPLICATION_ERROR(-20000, 'Error 20000: Error inesperado');    
+
+    END;
+/
+*/
